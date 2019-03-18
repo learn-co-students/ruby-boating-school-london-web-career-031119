4dbeb7cb5bd997787e6896362d0a6bec0e2b2166
oli-gra <oliver.grant@gmail.com>
2019-03-18 14:44:14 +0000

    done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,34 @@
 class Instructor
 
+  attr_reader :name, :student, :test_name, :test_status
+
+  ALL = []
+
+  def initialize(name)
+    @name = name
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def pass_student(student,test_name)
+    BoatingTest.all.each do |test|
+      if test.student == student && test.test_name == test_name
+        return test.test_status = 'passed'
+      end
+    end
+    BoatingTest.new(student,name,'passed',self)
+  end
+
+  def fail_student(student,test_name)
+    BoatingTest.all.each do |test|
+      if test.student == student && test.test_name == test_name
+        return test.test_status = 'failed'
+      end
+    end
+    BoatingTest.new(student,name,'failed',self)
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
 class Student
 
+attr_reader :first_name, :test_name, :test_status, :instructor
+
+ALL = []
+
+def initialize(first_name)
+  @first_name = first_name
+  ALL << self
+end
+
+def self.all
+  ALL
+end
+
+def add_boating_test(test_name,status,instructor)
+  # "Don't Crash 101", "pending", puff
+  BoatingTest.new(self,test_name,status,instructor)
+end
+
+def tests
+  BoatingTest.all.select{|test|test.student == self}
+end
+
+def self.find_student(name)
+  self.all.each do |student|
+    return student if student.first_name.downcase == name.downcase
+  end
+end
+
+def self.grade_percentage(student)
+# percentage of tests that the student has passed
+  tests = BoatingTest.all.select{|test| test.student == student}
+  score = tests.select{|test|test.test_status=='passed'}.count.to_f
+  return score / tests.count.to_f * 100
+end
+
 end
