e2d78dc6bbf066f08e462edbe731d8800d5fa729
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-18 14:39:59 +0000

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,38 @@
 class Instructor
+  attr_accessor :name
+  @@all = []
+
+  def pass_student(student, test_name)
+    student.tests.each do |test|
+      if test.test_name == test_name
+        test.test_status = "passed"
+        return
+      else
+        student.add_boating_test(test_name, "passed", self)
+        return
+      end
+    end
+  end
+
+  def fail_student(student, test_name)
+    student.tests.each do |test|
+      if test.test_name == test_name
+        test.test_status = "failed"
+        return
+      else
+        student.add_boating_test(test_name, "failed", self)
+        return
+      end
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,33 @@
 class Student
+  attr_accessor :first_name, :tests
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @tests = []
+    @@all << self
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    test = BoatingTest.new(self, test_name, test_status, instructor)
+    @tests << test
+  end
+
+  def grade_percentage
+    tests_passed = 0
+    @tests.each do |test|
+      tests_passed += 1 if test.test_status == "passed"
+    end
+    total_tests = @tests.count
+    pass_percentage = (tests_passed.to_f / total_tests.to_f) * 100.00
+    pass_percentage
+  end
+
+  def self.find_student(first_name)
+    @@all.find do |student|
+      student.first_name == first_name
+    end
+  end
+
 
 end
