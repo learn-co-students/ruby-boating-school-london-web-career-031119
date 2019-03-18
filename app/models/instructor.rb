212ace137276eef126181c826310c92a5b6e9fad
deeseert <gio.amms@gmail.com>
2019-03-18 16:29:07 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
+  attr_accessor :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+      if test
+        test.status == "passed"
+      else
+        BoatingTest.new(student, test_name, test_status, instructor)
+      end
+  end
+
+  def fail_student(student, test_name)
+    test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+      if test
+        test.status == "failed"
+      else
+        BoatingTest.new(student, test_name, test_status, instructor)
+      end
+  end
 
 end
+
+#
+# Instructor class:
+# Instructor#pass_student should take in a student instance and test name.
+# If there is a BoatingTest whose name and student match the values passed in,
+# this method should update the status of that BoatingTest to 'passed'.
+# If there is no matching test, this method should create a test with the student,
+# that boat test name, and the status 'passed'. Either way, it should return the BoatingTest instance.
+
+# Instructor#fail_student should take in a student instance and test name. Like #pass_student, it should try to find a matching BoatingTest and update its status to 'failed'. If it cannot find an existing BoatingTest, it should create one with the name, the matching student, and the status 'failed'.
--- tools/console.rb
+++ tools/console.rb
@@ -4,10 +4,33 @@ def reload
   load 'config/environment.rb'
 end
 
+#Students
+s1 = Student.new("Gio")
+s2 = Student.new("Patrick")
+s3 = Student.new("Fernando")
+s4 = Student.new("Diogo")
+spongebob = Student.new("Spongebob")
+patrick = Student.new("Patrick")
+#
+
+#Instructors
+i1 = Instructor.new("Guy")
+i2 = Instructor.new("Nico")
+i3 = Instructor.new("Vasile")
+i4 = Instructor.new("Avi")
+puff = Instructor.new("Ms.Puff")
+krabs = Instructor.new("Mr.Krabs")
+#
+s1.add_boating_test("new test", "passed", puff)
+#
+
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
