212ab7dfeea230ff3b36e07984703d99e386d866
peter-keller <keller.f.peter@gmail.com>
2019-03-18 14:33:19 +0000

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
+  attr_accessor :first_name
 
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def add_boating_test(course, status, teacher)
+    BoatingTest.new(self, course, status, teacher)
+  end
+
+  def grade_percentage
+    tests = BoatingTest.all.select { |boat_test| boat_test.student == self }
+    tests_passed = tests.select { |boat_test| boat_test.status == "passed" }
+    tests_passed.length / tests.length.to_f * 100
+  end
+
+  def self.find_student(name)
+    Student.all.select { |student| student.first_name == name }
+  end
+
+  def self.all
+    @@all
+  end
 end
