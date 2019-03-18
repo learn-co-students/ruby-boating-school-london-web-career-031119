159d9a5b8681770bbb1a96b5a055174764b8d753
github username <github email address>
2019-03-18 12:49:47 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_student_test(student, test_name)
+    student_records = BoatingTest.all.select {|t| t.student == student}
+    #returns an array of that student's test records
+    student_records.find {|r| r.test_name == test_name}
+  end
+
+  def pass_student(student, test_name)
+    if find_student_test(student, test_name) != nil
+      selected = find_student_test(student, test_name)
+      selected.status = "passed"
+    else
+      selected = BoatingTest.new(student: student, test_name: test_name, status: "passed", instructor: self)
+    end
+    selected
+  end
+
+  def fail_student(student, test_name)
+    if find_student_test(student, test_name) != nil
+      selected = find_student_test(student, test_name)
+      selected.status = "failed"
+    else
+      selected = BoatingTest.new(student: student, test_name: test_name, status: "failed", instructor: self)
+    end
+    selected
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(student: self, test_name: test_name, status: status, instructor: instructor)
+  end
+
+  def self.find_student(name)
+    BoatingTest.all.select {|test| test.student == name}
+  end
+
+  def grade_percentage
+    my_records = self.class.find_student(self)
+    my_status = my_records.map {|o| o.status}
+    my_passed = my_status.select {|s| s == "passed"}
+    ((my_passed.size / my_status.size.to_f) * 100).round(2)
+  end
+
 end
